9377f87ae16e5f6a9ae1502ca086e38cf11e3ed5
JaxRidd <115120113+jaxridd@users.noreply.github.com>
2025-03-02 23:53:35 -0600

    Finish 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	const sum = arr.reduce((total, num) => {
+    return total + num;
+  }, 0)
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  const multiplies = arr.reduce((total, num) => total * num, 1)
+  return multiplies;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let sum = 1
+	for(b; b > 0; b--) {
+    sum = sum * a;
+  }
+  return sum;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let sum = 1
+  for(a; a > 0; a--) {
+    sum = sum * a;
+  }
+  return sum;
 };
 
 // Do not edit below this line
