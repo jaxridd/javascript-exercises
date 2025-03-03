eba5a285d3031859651e3c7755ab6ec6b5704bb5
JaxRidd <115120113+jaxridd@users.noreply.github.com>
2025-03-03 16:34:36 -0600

    Finish 11 and 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const oldest = people.sort((a, b) => {
+        let lastGuy;
+        let nextGuy;
+        if (!a.yearOfDeath) {
+            lastGuy = new Date().getFullYear() - a.yearOfBirth;
+        } else {
+            lastGuy = a.yearOfDeath - a.yearOfBirth;
+        }
+        if (!b.yearOfDeath) {
+            nextGuy = new Date().getFullYear() - b.yearOfBirth;
+        } else {
+            nextGuy = b.yearOfDeath - a.yearOfBirth;
+        }
+        return lastGuy > nextGuy ? -1 : 1;
+    })
+    return oldest[0];
 };
 
 // Do not edit below this line
