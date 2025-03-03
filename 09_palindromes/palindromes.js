887c8a7c345f01cec9f4269cd57a73ee2ca44d87
JaxRidd <115120113+jaxridd@users.noreply.github.com>
2025-03-03 16:05:34 -0600

    Finish 09 and 10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    splited = str.split("")
+    splited.reverse()
+    reversedStr = splited.join("");
+    return str === reversedStr;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(countArg) {
 
+    let count;
+    if(typeof countArg !== 'number') {
+        count = parseInt(countArg);
+    } else {
+        count = countArg;
+    }
+
+    let firstPrev = 1;
+    let secondPrev = 0;
+
+    if (count < 0) return "OOPS";
+    if (count === 0) return 0;
+
+    for(let i = 2; i <= count; i++) {
+        let current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
+
+    return firstPrev;
 };
 
 // Do not edit below this line
